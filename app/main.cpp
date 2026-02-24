c53f9e0cea5325458d55fc962e63e00e7c73d2e4
evdaria <devseeva755@gmail.com>
2026-02-24 16:40:25 +0300

    feat: Добавлена генерация случайных кривых

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,66 @@
 #include <iostream>
 #include <memory>
+#include <vector>
+#include <random>
 #include "circle.h"
 #include "ellipse.h"
 #include "helix.h"
 
+const double MIN_RADIUS = 0.5;
+const double MAX_RADIUS = 10;
+const double MIN_STEP = 0.1;
+const double MAX_STEP = 5;
+
 int main() {
     std::cout << "Hello, Curves project!" << std::endl;
 
-    auto circle = std::make_unique<Circle>(5.0);
-    auto ellipse = std::make_unique<Ellipse>(2.5, 5.0);
+    std::vector<std::shared_ptr<Curve>> curves;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> type(0, 2);
+    std::uniform_real_distribution<> radius(MIN_RADIUS, MAX_RADIUS);
+    std::uniform_real_distribution<> step(MIN_STEP, MAX_STEP);
+
+    try {
+        bool circle = false;
+        bool ellipse = false;
+        bool helix = false;
+        while (!(circle && ellipse && helix)) {
+            int curveType = type(gen);
+
+            switch (curveType)
+            {
+            case 0: {
+                circle = true;
+                double circleRadius = radius(gen);
+                curves.push_back(std::make_shared<Circle>(circleRadius));
+                std::cout << "Circle: radius = " << circleRadius << std::endl;
+                break;
+            }
+            case 1: {
+                ellipse = true;
+                double ellipseRadiusX = radius(gen);
+                double ellipseRadiusY = radius(gen);
+                curves.push_back(std::make_shared<Ellipse>(ellipseRadiusX, ellipseRadiusY));
+                std::cout << "Ellipse: radiusX = " << ellipseRadiusX << ", radiusY = " << ellipseRadiusY << std::endl;
+                break;
+            }
+            case 2: {
+                helix = true;
+                double helixRadius = radius(gen);
+                double helixStep = step(gen);
+                curves.push_back(std::make_shared<Helix>(helixRadius, helixStep));
+                std::cout << "Helix: radius = " << helixRadius << ", step = " << helixStep << std::endl;
+                break;
+            }
+            }
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
